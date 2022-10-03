7fa19e49a2e3194ce5d6db4b91c94b54c5dd6d24
advballay <113952731+advballay@users.noreply.github.com>
2022-10-02 22:52:03 -0300

    primera vez

---
+++ main.js
@@ -0,0 +1,20 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="info">Html 5</i>',
+		'<i class="info">java script</i>',
+		'<i class="info">Php</i>',
+		'<i class="info">Css</i>'
+	],
+
+	typeSpeed: 75, 
+	startDelay: 300, 
+	backSpeed: 75, 
+	smartBackspace: true, 
+	shuffle: false, 
+	backDelay: 1500, 
+	loop: true, 
+	loopCount: false, 
+	showCursor: true, 
+	cursorChar: '|', 
+	contentType: 'html', 
+});
